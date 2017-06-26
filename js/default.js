ea644facf9f1602fc0386b0f4061cf5115f72685
shioncolor <czamy00@gmail.com>
2017-06-26 17:35:59 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -10,28 +10,23 @@ for(var i=1; i< word; i++)
 {
   if(word%3==0)
   {
-  war li=document.createElement('li');
   li.textContent='fizz';
   document.getElementById('list').appendChild(li);
   }
   else if(word%5==0)
   {
-  ar li=document.createElement('li');
   li.textContent='buzz';
   document.getElementById('list').appendChild(li);
   }
   else if(word%5==0 && word%3==0)
   {
-    ar li=document.createElement('li');
     li.textContent='fizz';
     document.getElementById('list').appendChild(li);
     
-    ar li=document.createElement('li');
     li.textContent='buzz';
     document.getElementById('list').appendChild(li);
   }
   else{
-    var li=document.createElement('li');
     li.textContent=word
     document.getElementById('list').appendChild(li);
   }
