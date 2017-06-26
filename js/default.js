0e85751c00db2c0936e5e007e7059772ac17ef8c
shioncolor <czamy00@gmail.com>
2017-06-26 17:48:20 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -6,32 +6,35 @@ document.getElementById('form').onsubmit=function(){
 window.alert(document.getElementById('form').word.value);
 };
 
-for(var i=1; i< word; i++)
-{
+var i=0;
+while(true){
+  i+=1;
   if(word%3==0)
   {
-    var li=document.createElement('li');
     li.textContent='fizz';
-    document.getElementById('list').appendChild(li)
+    document.getElementById('list').appendChild(li);
   }
   else if(word%5==0)
   {
-    var li=document.createElement('li');
     li.textContent='buzz';
-    document.getElementById('list').appendChild(li)
+    document.getElementById('list').appendChild(li);
   }
   else if(word%5==0 && word%3==0)
   {
-    var li=document.createElement('li');
     li.textContent='fizz';
-    document.getElementById('list').appendChild(li)
-    
-   var li=document.createElement('li');
+    document.getElementById('list').appendChild(li);
+   
     li.textContent='buzz';
-    document.getElementById('list').appendChild(li)
+    document.getElementById('list').appendChild(li);
   }
   else{
     li.textContent=word;
+    document.getElementById('list').appendChild(li);
+    }
+  
+  if(i==word)
+  {
+    break;
   }
 }
 
