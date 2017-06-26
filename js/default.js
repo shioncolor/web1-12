7bf1917798a1994f8b34d0b745e696e652b8c6c0
shioncolor <czamy00@gmail.com>
2017-06-26 17:23:16 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,8 +2,25 @@ var li=document.createElement('li');
 li.textContent='アイテム';
 document.getElementById('list').appendChild(li);
 
+for(var i=1; i<word; i++)
+{
 document.getElementById('form').onsubmit=function()
 {
-window.alert(document.getElementById('form').word.value);
+  if(word%3==0)
+  {
+  window.alert(document.getElementById('form').Fizz.value);
+  }
+  else if(word%5==0)
+  {
+  window.alert(document.getElementById('form').Buzz.value);
+  }
+  else if(word%5==0& & word%3==0)
+  {
+    window.alert(document.getElementById('form').Buzz.value);
+    window.alert(document.getElementById('form').Fizz.value);
+  }
+  else{
+window.alert(document.getElementById('form').i.value);
+  }
 };
-
+}
