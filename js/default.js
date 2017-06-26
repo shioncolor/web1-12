4f7322ed5c2793acf723601325cd01fc64a5cdc9
shioncolor <czamy00@gmail.com>
2017-06-26 17:37:07 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -11,24 +11,19 @@ for(var i=1; i< word; i++)
   if(word%3==0)
   {
   li.textContent='fizz';
-  document.getElementById('list').appendChild(li);
   }
   else if(word%5==0)
   {
   li.textContent='buzz';
-  document.getElementById('list').appendChild(li);
   }
   else if(word%5==0 && word%3==0)
   {
     li.textContent='fizz';
-    document.getElementById('list').appendChild(li);
     
     li.textContent='buzz';
-    document.getElementById('list').appendChild(li);
   }
   else{
-    li.textContent=word
-    document.getElementById('list').appendChild(li);
+    li.textContent=word;
   }
 }
 
