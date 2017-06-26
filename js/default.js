c16ab43536b838530e828d52f5ebe9d4a30b6196
shioncolor <czamy00@gmail.com>
2017-06-26 17:08:51 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,8 @@
 var li=document.createElement('li');
 li.textContent='アイテム';
 document.getElementById('list').appendChild(li);
+
+document.getElementById('form').onsubmit=function()
+{
+window.alert('押されました');
+};
