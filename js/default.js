311c2a1640f73222a0388b2adac1f7dd3f69d5bc
shioncolor <czamy00@gmail.com>
2017-06-26 18:33:49 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,26 +1,33 @@
 
 var i=0;
-var a;
-var word
+var word;
 
-
-a=word;
+var li.document.createElement('li');
 
 for(i=0; i<word; i++){
 
   if(a%3==0)
   {
-   System.out.println("fizz");
+   li.tectContent='fizz';
+   doument.getElementById('list').appendhild(li);
   }
   else if(a%5==0)
   {
-    System.out.println("buzz");
+    li.tectContent='buzz';
+   doument.getElementById('list').appendhild(li);
   }
   else if(a%5==0 && a%3==0)
   {
-    System.out.println("fizz");
-    System.out.println("buzz");
+   li.tectContent='fizz';
+   doument.getElementById('list').appendhild(li);
+    
+   li.tectContent='buzz';
+   doument.getElementById('list').appendhild(li);
   }
-  
+  else
+  {
+    li.tectContent=i;
+   doument.getElementById('list').appendhild(li);
+  }
 }
 
