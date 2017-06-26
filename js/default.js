442e8f238826aa05f56c3ccc3308c48a86732e8d
shioncolor <czamy00@gmail.com>
2017-06-26 18:38:15 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -6,17 +6,17 @@ var li=document.createElement('li');
 
 for(i=0; i<word; i++){
 
-  if(a%3==0)
+  if(word%3==0)
   {
    li.tectContent='fizz';
    doument.getElementById('list').appendhild(li);
   }
-  else if(a%5==0)
+  else if(word%5==0)
   {
     li.tectContent='buzz';
    doument.getElementById('list').appendhild(li);
   }
-  else if(a%5==0 && a%3==0)
+  else if(word%5==0 && word%3==0)
   {
    li.tectContent='fizz';
    doument.getElementById('list').appendhild(li);
